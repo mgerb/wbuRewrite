d2fa2db16c4df39e242e5f0e5acb4a296b9e334c
Mitchell <mgerb42@gmail.com>
2017-04-23 21:34:52 -0500

    groupSearch/userSearch updated

--- app/pages/GroupSearch.tsx
+++ app/pages/GroupSearch.tsx
@@ -1,6 +1,7 @@
-import _ from 'lodash';
 import React from 'react';
 import { View, ViewStyle, ScrollView, TextStyle, Text, TextInput, StyleSheet, TouchableHighlight, Keyboard} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import groupAPI from '../api/group.api';
 import { GroupType } from '../redux/reducers/group';
 import toast from '../utils/toast';
@@ -15,28 +16,37 @@ interface Props {
 interface State {
     searchGroup: string;
     groupList: Array<GroupType>;
+    noGroupsFound: boolean;
 }
 
 export default class GroupSearch extends React.Component<Props, State> {
 
-    private defaultState: State = {
-        searchGroup: "",
-        groupList: [],
-    };
-
     constructor() {
         super();
-        this.state = _.clone(this.defaultState);
+        this.state = {
+            searchGroup: "",
+            groupList: [],
+            noGroupsFound: false,
+        };
     }
 
     private fetchSearchPublicGroup() {
         if (this.state.searchGroup === "") {
             return;
         }
+
         Keyboard.dismiss();
+
+        this.setState({
+            groupList: [],
+            noGroupsFound: false,
+        });
+
         groupAPI.searchPublicGroups(this.state.searchGroup).then((response) => {
             this.setState({
+                searchGroup: "",
                 groupList: response.data,
+                noGroupsFound: response.data.length === 0,
             });
         }).catch(() => {
             toast.error("Error fetching groups.");
@@ -65,18 +75,30 @@ export default class GroupSearch extends React.Component<Props, State> {
                     </TouchableHighlight>
                 </View>
 
-                <ScrollView keyboardDismissMode={"on-drag"}>
-                    {this.state.groupList.map((group: GroupType, index: number) => {
-                        return (
-                            <TouchableHighlight underlayColor={colors.light1} key={index} onPress={() => this.navigateGroupInfo(group)}>
-                                <View style={styles.listItem}>
-                                    <Text style={[styles.listItemText, {fontSize: sizes.default}]}>{group.name}</Text>
-                                    <Text style={styles.listItemText}>{group.ownerEmail}</Text>
-                                </View>
-                            </TouchableHighlight>
-                        );
-                    })}
-                </ScrollView>
+                {this.state.noGroupsFound ?
+                    <View style={styles.noGroupsFoundView}>
+                        <Text style={styles.noGroupsFoundText}>No groups found.</Text>
+                    </View>
+                :
+                    <ScrollView keyboardDismissMode={"on-drag"}>
+                        {this.state.groupList.map((group: GroupType, index: number) => {
+                            let bottomBorder = index === this.state.groupList.length - 1 ? {borderBottomWidth: 0} : {};
+                            return (
+                                <TouchableHighlight key={index}
+                                                        underlayColor={colors.light1}
+                                                        onPress={() => this.navigateGroupInfo(group)}>
+                                    <View style={[styles.listItem, bottomBorder]}>
+                                        <View>
+                                            <Text style={[styles.listItemText, {fontSize: sizes.default}]}>{group.name}</Text>
+                                            <Text style={styles.listItemText}>{group.ownerEmail}</Text>
+                                        </View>
+                                        {group.locked ? <Icon name="lock" style={{fontSize: sizes.large, color: colors.primary}}/> : null}
+                                    </View>
+                                </TouchableHighlight>
+                            );
+                        })}
+                    </ScrollView>
+                }
             </View>
         );
     }
@@ -106,13 +128,24 @@ const styles = StyleSheet.create({
         color: colors.white,
     } as TextStyle,
     listItem: {
+        flexDirection: 'row',
         height: 50,
         borderBottomWidth: 1,
         borderColor: colors.light1,
-        justifyContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'space-between',
         paddingHorizontal: 20,
     } as ViewStyle,
     listItemText: {
+        color: colors.primary,
+    } as TextStyle,
+    noGroupsFoundView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    } as ViewStyle,
+    noGroupsFoundText: {
+        fontSize: sizes.default,
         color: colors.primary,
     } as TextStyle,
 });
--- app/pages/InviteUser.tsx
+++ app/pages/InviteUser.tsx
@@ -1,7 +1,8 @@
-import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import { View, ViewStyle, ScrollView, TextStyle, Text, TextInput, StyleSheet, TouchableHighlight, Keyboard } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import userAPI from '../api/user.api';
 import groupAPI from '../api/group.api';
 import { UserType } from '../redux/reducers/user';
@@ -9,43 +10,51 @@ import { GroupStateType } from '../redux/reducers/group';
 import toast from '../utils/toast';
 import navigation, { ClosableModal } from '../navigation';
 
+import colors from '../style/colors';
+import sizes from '../style/sizes';
+
 interface Props {
-    navigator: any,
-    group: GroupStateType,
+    navigator: any;
+    group: GroupStateType;
 }
 
 interface State {
-    searchUser: string,
-    userList: Array<UserType>,
+    searchUser: string;
+    userList: Array<UserType>;
+    noUsersFound: boolean;
 }
 
 class CreateGroup extends React.Component<Props, State> implements ClosableModal {
 
     static navigatorStyle = {...navigation.NavStyle};
 
-    private defaultState: State = {
-        searchUser: "",
-        userList: [],
-    };
-
     constructor(props: Props) {
         super(props);
-        this.state = _.clone(this.defaultState);
+        this.state = {
+            searchUser: "",
+            userList: [],
+            noUsersFound: false,
+        };
+
         this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
     }
 
     onNavigatorEvent(event: any): void {
-        if (event.type == "NavBarButtonPress") {
-            if (event.id == "close") {
-                this.props.navigator.dismissModal();
-            }
+        if (event.type === "NavBarButtonPress" && event.id === "close") {
+            this.props.navigator.dismissModal();
         }
     }
 
     private fetchSearchUserByName(): void {
+        if (this.state.searchUser === "") {
+            return;
+        }
+
         Keyboard.dismiss();
+
         this.setState({
             userList: [],
+            noUsersFound: false,
         });
 
         let searchUser = this.state.searchUser;
@@ -55,6 +64,7 @@ class CreateGroup extends React.Component<Props, State> implements ClosableModal
                 this.setState({
                     searchUser: "",
                     userList: response.data,
+                    noUsersFound: response.data.length === 0,
                 });
             });
         }
@@ -70,7 +80,7 @@ class CreateGroup extends React.Component<Props, State> implements ClosableModal
 
     public render() {
         return (
-            <View>
+            <View style={{flex:1}}>
                 <View style={styles.searchContainer}>
 
                     <TextInput placeholder="Search User"
@@ -78,19 +88,32 @@ class CreateGroup extends React.Component<Props, State> implements ClosableModal
                                style={styles.textInput}
                                value={this.state.searchUser}
                                onChangeText={(searchUser) => this.setState({searchUser})}/>
-                    <TouchableHighlight style={styles.submitButton} activeOpacity={50} underlayColor={'red'} onPress={ this.fetchSearchUserByName.bind(this) }>
-                        <Text>Search</Text>
+                    <TouchableHighlight style={styles.submitButton} underlayColor={colors.light1} onPress={ this.fetchSearchUserByName.bind(this) }>
+                        <Text style={styles.buttonText}>Search</Text>
                     </TouchableHighlight>
                 </View>
 
-                <ScrollView>
-
-                {this.state.userList.map((user: UserType, index: number) => {
-                    return <Text key={index} onPress={() => {this.fetchInviteUserToGroup(user.id)}}>
-                                {user.firstName + " " + user.lastName}
-                            </Text>
-                })}
-                </ScrollView>
+                {this.state.noUsersFound ?
+                    <View style={styles.noUsersFoundView}>
+                        <Text style={styles.noUsersFoundText}>No users found.</Text>
+                    </View>
+                :
+                    <ScrollView>
+                        {this.state.userList.map((user: UserType, index: number) => {
+                            let bottomBorder = index === this.state.userList.length - 1 ? {borderBottomWidth: 0} : {};
+                            return (
+                                <View key={index} style={[styles.listItem, bottomBorder]}>
+                                    <Text style={[styles.listItemText, {fontSize: sizes.default}]}>
+                                        {user.firstName + " " + user.lastName}
+                                    </Text>
+                                    <Icon name="plus"
+                                            style={{fontSize: sizes.large, color: colors.primary}}
+                                            onPress={() => {this.fetchInviteUserToGroup(user.id);}}/>
+                                </View>
+                            );
+                        })}
+                    </ScrollView>
+                }
             </View>
         );
     }
@@ -99,20 +122,49 @@ class CreateGroup extends React.Component<Props, State> implements ClosableModal
 const styles = StyleSheet.create({
     searchContainer: {
         flexDirection: 'row',
+        borderTopWidth: 2,
+        borderBottomWidth: 1,
+        borderColor: colors.light1,
+        height: 50,
+        paddingLeft: 20,
     } as ViewStyle,
     textInput: {
         flex: 1,
-        padding: 10,
         height: 50,
     } as TextStyle,
     submitButton: {
-        backgroundColor: "blue",
-        height: 50,
+        backgroundColor: colors.primary,
         width: 80,
         alignItems: "center",
         justifyContent: "center",
     } as ViewStyle,
+    buttonText: {
+        fontSize: sizes.default,
+        color: colors.white,
+    } as TextStyle,
+    listItem: {
+        flexDirection: 'row',
+        height: 50,
+        borderBottomWidth: 1,
+        borderColor: colors.light1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+    } as ViewStyle,
+    listItemText: {
+        color: colors.primary,
+    } as TextStyle,
+    noUsersFoundView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    } as ViewStyle,
+    noUsersFoundText: {
+        fontSize: sizes.default,
+        color: colors.primary,
+    } as TextStyle,
 });
+
 
 function mapStateToProps(state: Props): any {
   return {
